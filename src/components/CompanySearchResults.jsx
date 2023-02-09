ef581118801fb08b3b835a4d4dc828da10af3e01
Zaide Kurti <zaiido1920@gmail.com>
2023-02-09 17:15:05 +0100

    Code refactoring

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Job from './Job'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const CompanySearchResults = () => {
   const [jobs, setJobs] = useState([])
   const params = useParams()
+
+  const favourites = useSelector((state) => state.favourites.companies)
+
 
   const baseEndpoint = 'https://strive-benchmark.herokuapp.com/api/jobs?company='
 
@@ -33,7 +37,7 @@ const CompanySearchResults = () => {
       <Row>
         <Col>
           {jobs.map((jobData) => (
-            <Job key={jobData._id} data={jobData} />
+            favourites.includes(jobData.company_name) ? <Job key={jobData._id} data={jobData} favourite={true} /> : <Job key={jobData._id} data={jobData} favourite={false} />
           ))}
         </Col>
       </Row>
